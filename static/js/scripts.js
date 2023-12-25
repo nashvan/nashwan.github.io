b58d2068326248876c9c50204b1dfb58dade4d34
nashwan <nashwan.bilal@gmail.com>
2023-12-25 11:50:43 +1100

    2024 ghpage

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
 // const section_names = ['home', 'publications', 'awards', 'exams']
-const section_names = ['home', 'publications', 'awards', 'exams', 'ckad_core_concepts', 'configuration', 'multi_container_pod', 'observability', 'pod_design', 'services_and_networking', 'state_persistence']
+const section_names = ['home', 'publications', 'awards', 'exams', 'ckad/ckad_core_concepts', 'ckad/configuration', 'ckad/multi_container_pod', 'ckad/observability', 'ckad/pod_design', 'ckad/services_and_networking', 'ckad/state_persistence']
 
 
 window.addEventListener('DOMContentLoaded', event => {
@@ -49,18 +49,40 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
     // Marked
-    marked.use({ mangle: false, headerIds: false })
+    // marked.use({ mangle: false, headerIds: false })
+    // section_names.forEach((name, idx) => {
+    //     fetch(content_dir + name + '.md')
+    //         .then(response => response.text())
+    //         .then(markdown => {
+    //             const html = marked.parse(markdown);
+    //             document.getElementById(name + '-md').innerHTML = html;
+    //         }).then(() => {
+    //             // MathJax
+    //             MathJax.typeset();
+    //         })
+    //         .catch(error => console.log(error));
+    // })
+    
+    // Marked
+    marked.use({ mangle: false, headerIds: false });
     section_names.forEach((name, idx) => {
+        const [section, subSection] = name.split('/');
+
         fetch(content_dir + name + '.md')
             .then(response => response.text())
             .then(markdown => {
                 const html = marked.parse(markdown);
-                document.getElementById(name + '-md').innerHTML = html;
+                const targetElement = subSection ? document.getElementById(subSection + '-md') : document.getElementById(section + '-md');
+                if (targetElement) {
+                    targetElement.innerHTML = html;
+                } else {
+                    console.log("Unknown id: " + name);
+                }
             }).then(() => {
                 // MathJax
                 MathJax.typeset();
             })
             .catch(error => console.log(error));
-    })
+    });
 
 }); 
