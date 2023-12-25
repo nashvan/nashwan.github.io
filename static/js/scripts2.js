5436302173d0948c4e1ccab9a09259eb55633064
nashwan <nashwan.bilal@gmail.com>
2023-12-25 11:26:39 +1100

    2024 ghpage

--- static/js/scripts2.js
+++ static/js/scripts2.js
@@ -8,29 +8,6 @@ const section_names1 = ['ckad_core_concepts', 'configuration', 'multi_container_
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-
     // Yaml
     fetch(content_dir1 + config_file1)
         .then(response => response.text())
