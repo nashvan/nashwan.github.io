92bb4eff532e69ea6fd88212981dbaf03f97cb04
nashwan <nashwan.bilal@gmail.com>
2024-09-08 15:02:47 +1000

    fixed fetch .md file and load as html

--- static/js/article.js
+++ static/js/article.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const file = urlParams.get('file');
 
     if (file) {
-        fetch(`../../contents/articles/${file}`)
+        fetch(`articles/${file}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok.');
