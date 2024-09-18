71c7c209a46d872cef7e3609df244e2983010ac5
nashwan <nashwan.bilal@gmail.com>
2024-09-18 11:29:30 +1000

    updated marked library

--- static/js/article.js
+++ static/js/article.js
@@ -1,38 +1,6 @@
 // // import { marked } from '/static/js/node_modules/marked';
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const file = urlParams.get('file');
-
-//     if (file) {
-//         console.log(`Loading file: ${file}`);
-//         fetch(`contents/articles/${file}`)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(`Network response was not ok: ${response.statusText}`);
-//             }
-//             return response.text();
-//         })
-//         .then(markdown => {
-//             if (typeof marked !== 'undefined') {
-//                 const articleTitle = file.replace('.md', '').replace(/-/g, ' ');
-//                 document.getElementById('article-title').innerText = articleTitle;
-//                 const htmlContent = marked(markdown);
-//                 document.getElementById('article-content').innerHTML = htmlContent;
-//             } else {
-//                 throw new Error('marked.js is not loaded.');
-//             }
-//         })
-//         .catch(error => {
-//             document.getElementById('article-content').innerHTML = `Failed to load the article: ${error.message}`;
-//             console.error('Fetch operation failed:', error);
-//         });
-//     } else {
-//         document.getElementById('article-content').innerHTML = 'No article specified.';
-//     }
-// });
-
-
+const marked = require('marked');
 // Function to get URL query parameter
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
